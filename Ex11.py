e773b14f14e92e3a470974b5490cdeaf0156ce28
Svetlana Denezhkina <svetlana.denezhkina@epicsoftwaredev.com>
2021-07-06 12:38:45 +0300

    Ex11

---
+++ Ex11.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class TestEx11:
+    @staticmethod
+    def test_homework_cookie():
+        expected_cookie_value = "hw_value1"
+        expected_cookie_header = "HomeWork"
+        response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
+        cookies = response.cookies
+        # checking that expected header in returned cookies
+        assert expected_cookie_header in cookies, f"No header {expected_cookie_header} in response cookies: {cookies}"
+
+        # checking that expected cookie value returned
+        cookie_value = cookies[expected_cookie_header]
+        assert cookie_value == expected_cookie_value, f"Returned {expected_cookie_header}: {cookie_value} is not equal to expected {expected_cookie_value}"
